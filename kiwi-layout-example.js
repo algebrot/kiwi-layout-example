c1d8933d1112d9b09a5da3817545144ae2215651
algebrot <coraeacoleman@gmail.com>
2019-08-13 13:31:12 -0700

    fixed unsolveables and got some variable values to print right

--- kiwi-layout-example.js
+++ kiwi-layout-example.js
@@ -32,73 +32,93 @@ solver.addEditVariable(bRight, kiwi.Strength.strong);
 solver.addEditVariable(bWidth, kiwi.Strength.strong);
 
 // Create a variable calculated through a constraint
-// TODO: figure out why commented out constraints are unsolveable 
+// TODO: figure out why some variable values are not present 
 
 // 10 ≤ a.left - p.left 
 var aLMinusPLGE = new kiwi.Expression([-1, pLeft], aLeft, -10);
-solver.addConstraint(new kiwi.Constraint(aLMinusPLGE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(aLMinusPLGE, kiwi.Operator.Ge, kiwi.Strength.required));
 
 // a.left - p.left ≤ 10
 var aLMinusPLLE = new kiwi.Expression([-1, aLeft], pLeft, 10);
-// solver.addConstraint(new kiwi.Constraint(aLMinusPLLE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(aLMinusPLLE, kiwi.Operator.Le, kiwi.Strength.required));
 
 // 10 ≤ b.left - a.right
 var bLMinusARGE = new kiwi.Expression(bLeft, [-1, aRight], -10);
-solver.addConstraint(new kiwi.Constraint(bLMinusARGE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(bLMinusARGE, kiwi.Operator.Ge, kiwi.Strength.required));
 
 // b.left - a.right ≤ 10
 var bLMinusARLE = new kiwi.Expression(aRight, [-1, bLeft], 10);
-// solver.addConstraint(new kiwi.Constraint(bLMinusARLE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(bLMinusARLE, kiwi.Operator.Le, kiwi.Strength.required));
 
 // 10 ≤ p.right - b.right
 var pRMinusBRGE = new kiwi.Expression(pRight, [-1, bRight], -10);
-solver.addConstraint(new kiwi.Constraint(pRMinusBRGE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(pRMinusBRGE, kiwi.Operator.Ge, kiwi.Strength.required));
 
 // p.right - b.right ≤ 10
 var pRMinusBRLE = new kiwi.Expression(bRight, [-1, pRight], 10);
-// solver.addConstraint(new kiwi.Constraint(pRMinusBRLE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(pRMinusBRLE, kiwi.Operator.Le, kiwi.Strength.required));
 
 // 20 ≤ a.width 
 var aWidthGE = new kiwi.Expression(aWidth, -20);
-solver.addConstraint(new kiwi.Constraint(aWidthGE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(aWidthGE, kiwi.Operator.Ge, kiwi.Strength.required));
 
 // a.width ≤ 20
 var aWidthLE = new kiwi.Expression([-1, aWidth], 20);
-// solver.addConstraint(new kiwi.Constraint(aWidthLE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(aWidthLE, kiwi.Operator.Le, kiwi.Strength.required));
 
 // 30 ≤ b.width
 var bWidthGE = new kiwi.Expression(bWidth, -30);
-solver.addConstraint(new kiwi.Constraint(bWidthGE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(bWidthGE, kiwi.Operator.Ge, kiwi.Strength.required));
 
 // b.width ≤ 30
 var bWidthLE = new kiwi.Expression([-1, bWidth], 30);
-// solver.addConstraint(new kiwi.Constraint(bWidthLE, kiwi.Operator.Eq, kiwi.Strength.required));
+solver.addConstraint(new kiwi.Constraint(bWidthLE, kiwi.Operator.Le, kiwi.Strength.required));
+
+// Suggest some values to the solver
+solver.suggestValue(pLeft, 0);
+solver.suggestValue(pRight, 800);
+solver.suggestValue(pWidth, 800);
+
+solver.suggestValue(aLeft, 100);
+solver.suggestValue(aRight, 300);
+solver.suggestValue(aWidth, 200);
+
+solver.suggestValue(bLeft, 400);
+solver.suggestValue(bRight, 700);
+solver.suggestValue(bWidth, 300);
 
 // Lets solve the problem!
 solver.updateVariables();
 
 // Logger 
-console.log("pLeft "+pLeft.toString());
-console.log("pRight "+pRight.toString());
-console.log("pWidth "+pWidth.toString());
+console.log(" "); 
+console.log("Variable Values"); 
+console.log(" "); 
+console.log("pLeft "+pLeft.value().toString()); 
+console.log("pRight "+pRight.value().toString());
+console.log("pWidth "+pWidth.value().toString());
 
-console.log("aLeft "+aLeft.toString());
-console.log("aRight "+aRight.toString());
-console.log("aWidth "+aWidth.toString());
+console.log("aLeft "+aLeft.value().toString());
+console.log("aRight "+aRight.value().toString());
+console.log("aWidth "+aWidth.value().toString());
 
-console.log("bLeft "+bLeft.toString());
-console.log("bRight "+bRight.toString());
-console.log("bWidth "+bWidth.toString());
+console.log("bLeft "+bLeft.value().toString());
+console.log("bRight "+bRight.value().toString());
+console.log("bWidth "+bWidth.value().toString());
 
-console.log("10 ≤ a.left - p.left "+aLMinusPLGE.toString()); 
-console.log("a.left - p.left ≤ 10 "+aLMinusPLLE.toString()); 
-console.log("10 ≤ b.left - a.right "+bLMinusARGE.toString()); 
-console.log("b.left - a.right ≤ 10 "+bLMinusARLE.toString());  
-console.log("10 ≤ p.right - b.right "+pRMinusBRGE.toString());  
-console.log("p.right - b.right ≤ 10 "+pRMinusBRLE.toString()); 
-console.log("20 ≤ a.width "+aWidthGE.toString());  
-console.log("a.width ≤ 20 "+aWidthLE.toString()); 
-console.log("30 ≤ b.width "+bWidthGE.toString());  
-console.log("b.width ≤ 30 "+bWidthLE.toString());  
+console.log(" "); 
+console.log("Constraint Expressions"); 
+console.log(" "); 
+console.log("aLMinusPLGE  10 ≤ a.left - p.left  "+aLMinusPLGE.toString()); 
+console.log("aLMinusPLLE  a.left - p.left ≤ 10  "+aLMinusPLLE.toString()); 
+console.log("bLMinusARGE  10 ≤ b.left - a.right  "+bLMinusARGE.toString()); 
+console.log("bLMinusARLE  b.left - a.right ≤ 10  "+bLMinusARLE.toString());  
+console.log("pRMinusBRGE  10 ≤ p.right - b.right  "+pRMinusBRGE.toString());  
+console.log("pRMinusBRLE  p.right - b.right ≤ 10  "+pRMinusBRLE.toString()); 
+console.log("aWidthGE  20 ≤ a.width  "+aWidthGE.toString());  
+console.log("aWidthLE  a.width ≤ 20  "+aWidthLE.toString()); 
+console.log("bWidthGE  30 ≤ b.width  "+bWidthGE.toString());  
+console.log("bWidthLE  b.width ≤ 30  "+bWidthLE.toString());  
 
+console.log(" "); 
 console.log(solver);
